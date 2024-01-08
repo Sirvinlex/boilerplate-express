b85a61a1f3d091f9bb994e8f91b6d876a396d80e
Alex <vinlex3030@gmail.com>
2024-01-08 17:20:08 +0100

    updated task

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let app = express();
 
+
+const absolutePath = __dirname + "/views/index.html"
 const handler = (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(absolutePath);
   }
 app.get("/", handler)
 // console.log("Hello World")
