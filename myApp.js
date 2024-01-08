fe1310d40de2d1e9ce717c7db44a3ee296af0ecf
Alex <vinlex3030@gmail.com>
2024-01-08 18:26:58 +0100

    updated task

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
 let express = require('express');
 let app = express();
 
+// Normal usage
+app.use(express.static(__dirname + "/public"));
 
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 const absolutePath = __dirname + "/views/index.html"
 const handler = (req, res) => {
     res.sendFile(absolutePath);
