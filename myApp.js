0fbcb0469086460b07256309a99abc8dc5040050
Alex <vinlex3030@gmail.com>
2024-01-08 19:07:31 +0100

    updated task

--- myApp.js
+++ myApp.js
@@ -3,14 +3,19 @@ let app = express();
 
 // Normal usage
 app.use(express.static(__dirname + "/public"));
-
-// Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
+
 const absolutePath = __dirname + "/views/index.html"
 const handler = (req, res) => {
     res.sendFile(absolutePath);
-  }
+}
+const handler2 = (req, res) => {
+    res.json({
+        "message": "Hello json"
+    });
+}
 app.get("/", handler)
+app.get("/json", handler2)
 // console.log("Hello World")
 
 
